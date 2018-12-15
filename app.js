54fec3dcfb292071d98f5ab1018f46a5b965cc4c
Alexandr Rubnubnoff <rubnubnoff@yandex.ru>
2018-12-16 00:57:50 +0400

    q

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.use('/users', users);
 app.use('/tasks', tasks);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
